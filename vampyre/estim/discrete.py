fix infinite variance handling in GaussEst, DiscreteEst and LinEst
The infinite variance handling in GaussEst and DiscreteEst was broken,
because it passed avg_var_cost into the ind_out parameter of est_init().

Infinite variance handling was also added to LinEst and the unit tests for
all three estimators have been extended to test the infinite variance cases.

vampyre/estim/discrete.py
@@ -118,7 +118,7 @@ class DiscreteEst(BaseEst):
         
         # Infinite variance case
         if np.any(rvar==np.Inf):
-            return self.est_init(return_cost, avg_var_cost)
+            return self.est_init(return_cost=return_cost, ind_out=ind_out, avg_var_cost=avg_var_cost)
      
         
         # Convert to 1D vectors        

vampyre/estim/gaussian.py
@@ -133,7 +133,7 @@ class GaussEst(BaseEst):
         
         # Infinite variance case
         if np.any(rvar==np.Inf):
-            return self.est_init(return_cost, avg_var_cost)
+            return self.est_init(return_cost=return_cost, ind_out=ind_out, avg_var_cost=avg_var_cost)
                     
         zhatvar = rvar*self.zvar/(rvar + self.zvar)
         gain = self.zvar/(rvar + self.zvar)