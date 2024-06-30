fix infinite variance handling of ReLUEst
The infinite variance code path was broken, because the zhat0_map and
related variables did not get set in this code path. The relu unit tests
have been extendet to test this code path.

test/test_estim/test_relu.py
@@ -13,7 +13,7 @@ env.add_vp_path()
 import vampyre as vp
 
 
-def relu_test(zshape=(1000,10),tol=0.15,verbose=False,map_est=False):
+def relu_test(zshape=(1000,10),tol=0.15,verbose=False,map_est=False,rvar1=None):
     """
     ReLUEstim unit test.
     
@@ -33,7 +33,8 @@ def relu_test(zshape=(1000,10),tol=0.15,verbose=False,map_est=False):
         
     # Set random parameters
     rvar0 = np.power(10,np.random.uniform(-2,1,ns))
-    rvar1 = np.power(10,np.random.uniform(-2,1,ns))
+    if rvar1 is None:
+        rvar1 = np.power(10,np.random.uniform(-2,1,ns))
         
     # Construct random input
     r0 = np.random.normal(0,1,zshape) 
@@ -85,6 +86,9 @@ class TestCases(unittest.TestCase):
         
         # MAP test. we use a higher tolerance, since the error is approximate        
         relu_test(verbose=verbose,map_est=True,tol=1)        
+
+        # MMSE test with infinite rvar1 variance
+        relu_test(verbose=verbose,map_est=False,tol=0.15,zshape=(1000,10),rvar1=np.inf*np.ones(10))
         
 if __name__ == '__main__':    
     unittest.main()

vampyre/estim/relu.py
@@ -193,6 +193,11 @@ class ReLUEst(BaseEst):
         rvar0 = common.repeat_axes(rvar0,self.shape[0],self.var_axes[0])
         rvar1 = common.repeat_axes(rvar1,self.shape[1],self.var_axes[1])
         
+        # Compute the MAP estimate
+        zhat_map, zvar_map = self.est_map(r,rvar,return_cost=False,ind_out=[0,1])
+        zhat0_map, zhat1_map = zhat_map
+        zvar0_map, zvar1_map = zvar_map
+
         if np.any(rvar1 == np.Inf):
             # Infinite variance case.
             zvarp = rvar0
@@ -204,11 +209,6 @@ class ReLUEst(BaseEst):
             Amax = 0
                         
         else:
-            
-            # Compute the MAP estimate
-            zhat_map, zvar_map = self.est_map(r,rvar,return_cost=False,ind_out=[0,1])
-            zhat0_map, zhat1_map = zhat_map
-            zvar0_map, zvar1_map = zvar_map
                             
             # Compute the conditional Gaussian terms for z > 0 and z < 0
             zvarp = rvar0*rvar1/(rvar0+rvar1)